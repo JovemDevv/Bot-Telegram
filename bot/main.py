8da09c9babdf94471259e138e945753f247958e0
Ana Caroline Vasconcellos <101136752+JovemDevv@users.noreply.github.com>
2023-01-02 13:36:17 -0300

    Add files via upload

--- bot/main.py
+++ bot/main.py
@@ -1,4 +1,3 @@
-from pyexpat.errors import messages
 from dotenv import load_dotenv
 import requests
 import json
@@ -14,30 +13,43 @@ class TelegramBot:
     def iniciar(self):
         update_id = None
         while True:
-            atualizacao = self.obter_mensagens = (update_id)
-            mensagens = atualizacao ['resultado']
-            if mensagens:
-                for mensagem in mensagens:
-                    update_id = mensagem['update_id']
-                    chat_id = mensagem['mensagem']['from']['id']
-                    resposta_bot  = mensagem['mensagem']['texto']
-                    resposta = self.criar_resposta(chat_id, resposta_bot)
-                    self.responder(resposta,chat_id)
-    
-    def obter_mensagens(self,update_id):
-        link_request = f'{self.url_base}getUpdates?timeout+1000oddset='
+            atualizacao = self.obter_mensagens(update_id)
+            if atualizacao:  # verifica se atualizacao é diferente de None
+                mensagens = atualizacao['resultado']
+                if mensagens:
+                    for mensagem in mensagens:
+                        update_id = mensagem['update_id']
+                        chat_id = mensagem['mensagem']['from']['id']
+                        texto = mensagem['mensagem']['text']
+                        resposta = self.criar_resposta(texto)
+                        self.responder(resposta,chat_id)
+
+    def obter_mensagens(self, update_id):
+        link_request = f'{self.url_base}getUpdates?timeout=1000&offset='
         resultado = requests.get(link_request)
         return json.loads(resultado.content)
 
-    def criar_resposta(self, mensagem, resposta_bot):
-        mensagem = mensagem['mensagem']['texto']
-        if resposta_bot == True or mensagem.lower() == 'voltar':
-           return f'''Olá bem vindo ao Hora de Estudar. Digite o dia
-           da semana{os.linesep} 1 - Segunda{os.linesep}
-           2 - Terça{os.linesep} 3 - Quarta{os.linesep} 4 - Quinta{os.linesep}
-           5 - Sexta{os.linesep}'''
-       
 
+    def criar_resposta(self, texto):
+        if texto.lower() == 'voltar':
+            return f'''Olá bem vindo ao Hora de Estudar. Digite o dia
+            da semana{os.linesep} 1 - Segunda{os.linesep}
+            2 - Terça{os.linesep} 3 - Quarta{os.linesep} 4 - Quinta{os.linesep}
+            5 - Sexta{os.linesep}'''
+        elif texto.lower() == '1':
+            return 'Segunda: Python'
+        elif texto.lower() == '2':
+            return 'Terça: HTML e CSS'
+        elif texto.lower() == '3':
+            return 'Quarta: JavaScript'
+        elif texto.lower() == '4':
+            return 'Quinta: Inglês'
+        elif texto.lower() == '5':
+            return 'Sexta: Algoritmos'
+        else:
+            return 'Desculpe, eu não entendi o que você quer dizer. Por favor, digite novamente.'
+
+        
     def responder(self, resposta,chat_id):
         link_de_envio = f"{self.url_base}sendMessage?chat_id={chat_id}&text={resposta}"
         requests.get(link_de_envio)
